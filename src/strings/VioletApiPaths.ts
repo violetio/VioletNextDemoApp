40d24b874559838e09522845f1c6e2554a23c050
Andy Chin <andy.chin@violet.io>
2023-03-15 06:19:12 -1000

    Replace all api routes with a generic catchall API route which replaces /api with /v1 and forwards the request to Violet API with the required headers + token

--- src/strings/VioletApiPaths.ts
+++ src/strings/VioletApiPaths.ts
@@ -1,4 +1,4 @@
-const endpointPrefix = `${process.env.API_ENDPOINT}/v1`;
+export const endpointPrefix = `${process.env.API_ENDPOINT}/v1`;
 
 export const loginEndpoint = `${endpointPrefix}/login`;
 export const productsEndpoint = `${endpointPrefix}/catalog/products`;
