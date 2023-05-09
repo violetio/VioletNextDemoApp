0e4179e07fa298e67bb683db936e671b479ee93b
ishan-violet <ishan.guru@violet.io>
2023-05-09 15:43:58 -0400

    Checkout form should not hardcode to https

--- src/components/CheckoutForm/CheckoutForm.tsx
+++ src/components/CheckoutForm/CheckoutForm.tsx
@@ -100,7 +100,7 @@ const CheckoutForm = ({ fullApplePayCheckout }: Props) => {
       //`Elements` instance that was used to create the Payment Element
       elements,
       confirmParams: {
-        return_url: `https://${window.location.host}/paymentAccepted?cartId=${
+        return_url: `${window.location.protocol}//${window.location.host}/paymentAccepted?cartId=${
           cartState.order?.id
         }&${new URLSearchParams(
           router.query as Record<string, string>
